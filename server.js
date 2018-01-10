ffa7c4be2b74dd50911db7dddeb6df5e3f491b45
jonathan1313 <jonathanwillianevo@hotmail.com>
2018-01-10 01:03:32 -0300

    Fundaçao de edição(update/put) criada

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
-app.set('view engine', 'ejs');
+var db
 
+MongoClient.connect('mongodb://jonathan:glbk32@ds133597.mlab.com:33597/star-wars-crud', (err, database) => {
+    if (err) return console.log(err);
+
+    db = database;
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })  
+})
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {   
     db.db('star-wars-crud').collection('quotes').find().toArray(function(err, result) {
@@ -24,13 +37,24 @@ app.post('/quotes', (req, res) => {
     })
 })
 
-var db
-
-MongoClient.connect('mongodb://jonathan:glbk32@ds133597.mlab.com:33597/star-wars-crud', (err, database) => {
-    if (err) return console.log(err);
-
-    db = database;
-    app.listen(3000, () => {
-        console.log('listening on 3000')
-    })  
+app.put('/quotes', (req, res) => {
+    db.db('star-wars-crud').collection('quotes').findOneAndUpdate(
+        {
+            name: 'Yoda'
+        },
+        {
+            $set: {
+                name: req.body.name,
+                quote: req.body.quote
+            }
+        },
+        {
+            sort: {_id:-1},
+            upsert: true
+        },
+        (err, result) => {
+            if(err) return res.send(err)
+            res.send(result)
+        }
+    )
 })
